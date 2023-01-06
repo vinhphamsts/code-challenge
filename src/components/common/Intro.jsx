c5c70d629369b31c1b4b6a897dd4f47731695b1c
Vinh Pham Thanh <vinh.pham@saigontechnology.com>
2023-01-06 13:30:42 +0700

    chore: Update intro

--- src/components/common/Intro.jsx
+++ src/components/common/Intro.jsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
+import { LIGHT_COLOR, PASSED_COLOR } from '../../styles/colors.js';
+
 const Container = styled.div`
   position: relative;
   font-size: 16px;
   padding: 16px;
-  color: ${MAIN_COLOR};
+  color: ${LIGHT_COLOR};
   margin-top: 12px;
+  text-shadow: 1px 1px 2px 2px;
 
   &:before {
     position: absolute;
@@ -14,25 +17,24 @@ const Container = styled.div`
     top: 8px;
     border-top: 8px solid ${PASSED_COLOR};
   }
-	
+
   @media screen and (min-width: 768px) {
-    font-size: 22px;
+    max-width: 80%;
+    font-size: 18px;
     margin-top: 0;
 
     &:before {
       width: 5%;
     }
   }
-
 `;
 
-import { MAIN_COLOR, PASSED_COLOR } from '../../styles/colors.js';
 export const Intro = () => {
 	return (
 		<Container>
 			<blockquote>
-				<i>Challenge yourself on small coding exercises and improve your development skill by training with
-				your peers on code challenge that continuously challenge and push your coding practice. Let get started!</i>
+				<i>"Challenge yourself on small coding exercises and improve your development skill by training with
+				your peers on code challenge that continuously challenge and push your coding practice. Let get started!"</i>
 			</blockquote>
 		</Container>
 	);
